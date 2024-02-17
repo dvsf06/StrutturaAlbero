56cfee709dcf3a0c6df88cdad918b87d6d0fe0fc
davide.sforza.s1_giu <davide.sforza.s1_giu@LABGIOVEP1>
2024-02-17 09:29:08 +0100

    Aggiungere i file di progetto.

---
+++ StrutturaAlbero/Albero.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrutturaAlbero
+{
+    public class Albero
+    {
+        Nodo radice;
+
+        public Albero(string Valore)
+        {
+            radice = new Nodo(Valore);
+        }
+
+        public void AggiungiNodo(string Valore, int IdNodoPrecedente)
+        {
+            Nodo x = new Nodo(Valore);
+        }
+    }
+}
---
+++ StrutturaAlbero/Nodo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrutturaAlbero
+{
+    public class Nodo
+    {
+        static int nextId = 0;
+
+        int idNodo;
+        string valore;
+        List<Nodo> nodiSuccessivi;
+
+        public Nodo(string Valore)
+        {
+            idNodo = nextId;
+            valore = Valore;
+            nodiSuccessivi = new List<Nodo>();
+            nextId++;
+        }
+
+        public int IdNodo { get => idNodo; set => idNodo = value; }
+        public string Valore { get => valore; set => valore = value; }
+        public List<Nodo> NodiSuccessivi { get => nodiSuccessivi; set => nodiSuccessivi = value;}
+    }
+}
